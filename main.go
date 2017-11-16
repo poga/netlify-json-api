5bddf26325275568bacba778f560520dbc041e19
Poga Po <poga.bahamut@gmail.com>
2017-11-17 07:44:07 +0800

    Revert "filter column"
    This reverts commit 6947b9df1818ac4baeb07f1d762b324bc0c90b1a.

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ func main() {
 	resourceType := flag.String("type", "", "resource type name")
 	outputPath := flag.String("out", "", "output directory")
 	idKeys := flag.String("id", "id", "ID column names (seperate by \",\")")
-	filterKeys := flag.String("filter", "", "enable filtering on columns (seperate by \",\")")
 	perPage := flag.Int("perPage", 10, "items per page")
 	flag.Parse()
 	if *host == "" {
@@ -37,7 +36,6 @@ func main() {
 		resourceType = &inferType
 	}
 	idKeysSlice := strings.Split(*idKeys, ",")
-	filterKeysSlice := strings.Split(*filterKeys, ",")
 
 	f, err := os.Open(*file)
 	if err != nil {
@@ -72,7 +70,7 @@ func main() {
 			log.Fatal(err)
 		}
 		out := filepath.Join(*outputPath, fmt.Sprintf("%s-%d.json", *resourceType, i))
-		err = ioutil.WriteFile(out, bytes, 0666)
+		err = ioutil.WriteFile(out, bytes, 0664)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -87,47 +85,22 @@ func main() {
 		outDir := filepath.Join(*outputPath, *resourceType)
 		os.MkdirAll(outDir, os.ModePerm)
 		out := filepath.Join(outDir, fmt.Sprintf("%s.json", doc.Data[0].ID))
-		err = ioutil.WriteFile(out, bytes, 0666)
+		err = ioutil.WriteFile(out, bytes, 0664)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-	// build _redirects
+
+	// build rewrites
 	rewrites := fmt.Sprintf("/%s.json page=:p /%s-:p.json 200!\n", *resourceType, *resourceType)
 	rewrites += fmt.Sprintf("/%s.json /%s-0.json 200!\n", *resourceType, *resourceType)
-
-	for _, filterKey := range filterKeysSlice {
-		filteredDocs := buildFilteredDocumentsForColumn(*host, *resourceType, header, rows, idKeysSlice, *perPage, filterKey)
-
-		for idx, docs := range filteredDocs {
-			for i, doc := range docs {
-				outDir := filepath.Join(*outputPath, *resourceType+"-filtered", idx)
-				os.MkdirAll(outDir, os.ModePerm)
-				out := filepath.Join(outDir, fmt.Sprintf("%s-%d.json", idx, i))
-				file, err := os.Create(out)
-				if err != nil {
-					log.Fatal(err)
-				}
-				enc := json.NewEncoder(file)
-				enc.SetEscapeHTML(false)
-				err = enc.Encode(doc)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-			rewrites += fmt.Sprintf("/%s.json filter[%s]=:filter page=:p /%s-filtered/:filter/%s-:p.json 200!\n", *resourceType, *resourceType, filterKey, idx)
-			rewrites += fmt.Sprintf("/%s.json filter[%s]=:filter /%s-filtered/:filter/%s-0.json 200!\n", *resourceType, *resourceType, filterKey, idx)
-		}
-
-	}
-
-	ioutil.WriteFile(filepath.Join(*outputPath, "_redirects"), []byte(rewrites), 0666)
+	ioutil.WriteFile(filepath.Join(*outputPath, "_redirects"), []byte(rewrites), 0664)
 
 	// build _headers
 	headers := fmt.Sprintf("/*")
 	headers += fmt.Sprintf("  Access-Control-Allow-Origin: *")
 	headers += fmt.Sprintf("  content-type: application/json; charset=utf-8")
-	ioutil.WriteFile(filepath.Join(*outputPath, "_headers"), []byte(headers), 0666)
+	ioutil.WriteFile(filepath.Join(*outputPath, "_headers"), []byte(headers), 0664)
 }
 
 type Document struct {
@@ -140,61 +113,6 @@ type Object struct {
 	Type       string                 `json:"type"`
 	ID         string                 `json:"id"`
 	Attributes map[string]interface{} `json:"attributes"`
-}
-
-func buildFilteredDocumentsForColumn(host string, objType string, header []string, rows [][]string, idKeys []string, pageSize int, filterKey string) map[string][]Document {
-	indexedObjects := make(map[string][]Object)
-	indexedDocs := make(map[string][]Document)
-
-	for _, row := range rows {
-		obj := buildObject(objType, header, row, idKeys)
-		idx := obj.Attributes[filterKey].(string)
-		if _, exists := indexedObjects[idx]; !exists {
-			indexedObjects[idx] = make([]Object, 0)
-		}
-		indexedObjects[idx] = append(indexedObjects[idx], obj)
-
-		if len(indexedObjects[idx]) == pageSize {
-			doc := Document{}
-			doc.Data = indexedObjects[idx]
-			if _, exists := indexedDocs[idx]; !exists {
-				indexedDocs[idx] = make([]Document, 0)
-			}
-			indexedDocs[idx] = append(indexedDocs[idx], doc)
-			indexedObjects[idx] = make([]Object, 0)
-		}
-	}
-	for idx, objs := range indexedObjects {
-		if len(objs) > 0 {
-			doc := Document{}
-			doc.Data = objs
-			if _, exists := indexedDocs[idx]; !exists {
-				indexedDocs[idx] = make([]Document, 0)
-			}
-			indexedDocs[idx] = append(indexedDocs[idx], doc)
-		}
-	}
-	// set meta & links
-	for idx, docs := range indexedDocs {
-		for i, doc := range docs {
-			doc.Meta = map[string]interface{}{
-				"total-pages": len(docs),
-			}
-
-			links := make(map[string]string)
-			if i > 0 {
-				links["prev"] = fmt.Sprintf("%s/%s.json?filter[%s]=%s&page=%d", host, objType, filterKey, idx, i-1)
-			}
-			if i < len(docs)-1 {
-				links["next"] = fmt.Sprintf("%s/%s.json?filter[%s]=%s&page=%d", host, objType, filterKey, idx, i+1)
-			}
-
-			links["first"] = fmt.Sprintf("%s/%s.json?filter[%s]=%s&page=0", host, objType, filterKey, idx)
-			links["last"] = fmt.Sprintf("%s/%s.json?filter[%s]=%s&page=%d", host, objType, filterKey, idx, len(docs)-1)
-			docs[i].Links = links
-		}
-	}
-	return indexedDocs
 }
 
 func buildPaginatedIndexDocuments(host string, objType string, header []string, rows [][]string, idKeys []string, pageSize int) []Document {
